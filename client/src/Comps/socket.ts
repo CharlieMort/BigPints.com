7bc9bfbbc819b3cebe4e984ba7d7a4f25d426eb3
CharlieMort <charlie_mort@outlook.com>
2025-01-15 22:30:06 +0000

    asdasd

--- client/src/Comps/socket.ts
+++ client/src/Comps/socket.ts
@@ -1,4 +1,4 @@
-const  debug = true;
+const  debug = false;
 export const SOCKURL = debug ? "ws://localhost:80/ws" : "wss://bigpints.com/ws" 
 export const API_URL = debug ? "http://localhost:80" : "https://bigpints.com"
 export let socket = new WebSocket(SOCKURL)
